631e677e72bb9cca8a49ffccfa61442ada882f77
Morrnor <morris.morrnor@gmail.com>
2025-01-22 20:33:51 +0100

    changes

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -185,6 +185,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/morrisnordqvist/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  vimtex = {
+    loaded = true,
+    path = "/Users/morrisnordqvist/.local/share/nvim/site/pack/packer/start/vimtex",
+    url = "https://github.com/lervag/vimtex"
   }
 }
 
